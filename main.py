19810679acccf197817a079ae41d937bb8b83df9
PaulaAndreaPobladoGarcia <182712699+PaulaAndreaPobladoGarcia@users.noreply.github.com>
2024-11-05 15:06:10 -0500

    Solution exercise 4

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-voltaje1 = float (input(" ingrese el primer voltaje: "))
-voltaje2 =float (input(" ingrese el segundo voltaje: "))
-voltaje3 =float (input(" ingrese el tercer voltaje: "))
-VoltajeTotal = (voltaje1 + voltaje2+ voltaje3) /3
- 
-if VoltajeTotal > 220:
-     print(F"!PELIGRO¡") 
-elif VoltajeTotal >= 116:
-     print(F"alto voltaje")
-     print(f"{VoltajeTotal}")
-else: 
-      print(F"Voltaje corecto")
-      print(f"{VoltajeTotal}")
+m = float(input("ingresa el distancia en metros: "))
+
+km = m / 1000  
+
+print(f"""El resultado de la conversion es: 
+                   {km} km""")
