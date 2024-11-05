80e89b687aba46203b1ffb778152c979607071fd
PaulaAndreaPobladoGarcia <182712699+PaulaAndreaPobladoGarcia@users.noreply.github.com>
2024-11-05 15:01:01 -0500

    Solution exercise 3

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-medidasTriangulo = float(input("ingrese la medida de su cuadro equilatero: "))
-lado = medidasTriangulo * medidasTriangulo
-r = 1.73
-area = lado * r / 4
-totalArea = area
-
-if totalArea > 1000:
-    print(f"Datos no validos")
-
-else:
-    print(f"el area de su rectangulo es {totalArea}")
+voltaje1 = float (input(" ingrese el primer voltaje: "))
+voltaje2 =float (input(" ingrese el segundo voltaje: "))
+voltaje3 =float (input(" ingrese el tercer voltaje: "))
+VoltajeTotal = (voltaje1 + voltaje2+ voltaje3) /3
+ 
+if VoltajeTotal > 220:
+     print(F"!PELIGRO¡") 
+elif VoltajeTotal >= 116:
+     print(F"alto voltaje")
+     print(f"{VoltajeTotal}")
+else: 
+      print(F"Voltaje corecto")
+      print(f"{VoltajeTotal}")
